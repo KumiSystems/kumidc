b119f1ef7118f1a71a66f153712d59d1a7c9b229
Kumi <git@kumi.email>
2022-08-02 10:29:58 +0200

    Initial commit

---
+++ kumidc/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path, re_path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    re_path(r'^openid/', include('oidc_provider.urls', namespace='oidc_provider')),
+]
