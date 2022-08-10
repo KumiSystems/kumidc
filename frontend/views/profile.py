9a8d5eb859711a88eac704cb363c530edf540e74
Kumi <git@kumi.email>
2022-08-10 07:22:40 +0000

    Fix success URL for profile view

--- frontend/views/profile.py
+++ frontend/views/profile.py
@@ -1,4 +1,5 @@
 from django.views.generic import UpdateView
+from django.urls import reverse_lazy
 
 from core.models.profile import Profile
 
@@ -23,3 +24,5 @@ class OwnProfileView(ProfileUpdateView):
 
     def get_object(self, queryset=None):
         return self.request.user.profile
+
+    success_url = reverse_lazy("frontend:profile")
